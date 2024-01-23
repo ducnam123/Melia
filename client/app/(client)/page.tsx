e91b55dc2fe65c41e0da7da2a4ba1fd479a10080
Alone <tranluong460@gmail.com>
2024-01-23 10:40:44 +0700

    refs: DATN-5 feat" up clerk

--- client/app/(client)/page.tsx
+++ client/app/(client)/page.tsx
@@ -1,5 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Trang chủ",
+  description:
+    "Khách sạn Melia Hà Nội. Khám phá bữa sáng tuyệt vời. ✔ Wi-Fi miễn phí ✔ Chỗ đậu xe miễn phí ✔ Gym ✔ Spa ✔ Liệu pháp | Ưu đãi Melia Rewards độc quyền",
+};
+
 const HomePage = () => {
-  return <div className="text-primary">HomePage</div>;
+  return <div>HomePage</div>;
 };
 
 export default HomePage;
