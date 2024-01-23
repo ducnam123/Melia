1cc3c23d83fde246f8d2a76b3c1e6e9b452d8be0
Alone <tranluong460@gmail.com>
2024-01-23 13:32:28 +0700

    fix

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,8 +2,6 @@ import http from "http";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import favicon from "serve-favicon";
-import appRoot from "app-root-path";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 
@@ -26,8 +24,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(favicon(`${appRoot}/public/logo.svg`));
-
 const server = http.createServer(app);
 
 server.listen(process.env.NODE_PUBLIC_PORT || 8080, () => {
@@ -35,9 +31,7 @@ server.listen(process.env.NODE_PUBLIC_PORT || 8080, () => {
 });
 
 connect();
-app.use("/", (req, res) => {
-  res.send("Hello World!");
-});
+
 app.use("/api", baseRouter());
 app.use(notFoundRoute);
 app.use(errorHandler);
