9b72b817bd7729b805a3b2b96feff363133fa6f1
Alone <tranluong460@gmail.com>
2024-01-23 13:29:55 +0700

    up favicon

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,8 @@ import http from "http";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import favicon from "serve-favicon";
+import appRoot from "app-root-path";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 
@@ -15,11 +17,16 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
+
 app.use(cookieParser());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(favicon(`${appRoot}/public/logo.svg`));
 
 const server = http.createServer(app);
 
@@ -28,7 +35,9 @@ server.listen(process.env.NODE_PUBLIC_PORT || 8080, () => {
 });
 
 connect();
-
+app.use("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.use("/api", baseRouter());
 app.use(notFoundRoute);
 app.use(errorHandler);
