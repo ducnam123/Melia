681035defee1f933316bfcee6a8cd3a409481d4b
Alone <tranluong460@gmail.com>
2024-01-23 13:59:27 +0700

    up favicon

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,9 @@
+import path from "path";
 import http from "http";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import favicon from "serve-favicon";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 
@@ -27,6 +29,9 @@ if (process.env.NODE_PUBLIC_ENV === "development") {
 app.use(cors());
 
 app.use(cookieParser());
+
+const publicPath = path.join(__dirname, "../public");
+app.use(favicon(path.join(publicPath, "favicon.ico")));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
